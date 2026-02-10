23335f78154f62bbcb9cb19dda155d44860e63bd
JR Escert <uhoneprincemulaudzi@gmail.com>
2026-02-10 15:50:59 +0200

    added a servlet

---
+++ MessageEncrypterWebApp/src/java/za/ac/tut/web/EncryptMessageServlet.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.web;
+
+import ac.za.tut.entities.MessageEncrypter;
+import ac.za.tut.module.bl.MessageEncryptorInt;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author JREscert
+ */
+public class EncryptMessageServlet extends HttpServlet {
+   @EJB
+   private MessageEncryptorInt me;
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String message = request.getParameter("message");
+        
+       MessageEncrypter entity = me.encryptMessage(message);
+      
+       //return results to the user
+       request.setAttribute("message",entity.getMessage());
+       request.setAttribute("encrypted",entity.getEncryptedMessage());
+       
+       //request dispatcher 
+       RequestDispatcher disp = request.getRequestDispatcher("encrypted_message_outcome.jsp");
+       disp.forward(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
